f624cfd3b598c061803bb1b1d4aa92aa27366988
soumya16kp <soumya16kp@gmail.com>
2025-06-02 17:36:08 +0530

    modified:   Project_2/Project_2/settings.py

--- Project_2/Project_2/settings.py
+++ Project_2/Project_2/settings.py
@@ -36,7 +36,8 @@ if ENVIRONMENT=='development':
 else:
     DEBUG=False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','*']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','chai-aur-code-yiqj.onrender.com']
+CSRF_TRUSTED_ORIGINS=['https://chai-aur-code-yiqj.onrender.com']
 
 
 # Application definition
